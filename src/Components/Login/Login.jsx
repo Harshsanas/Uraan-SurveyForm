1db898c7a36a4ed145b2b9745f4346f53f2bd8d3
Harshsanas <harrysanas140@gmail.com>
2021-10-25 20:08:27 +0530

    login css

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,31 @@
 import React, {useContext, useState} from 'react'
 import { Redirect } from 'react-router'
 import {AuthContext} from "../AuthContext/AuthContext"
+import styled from "styled-components";
+
+const FORMDATA = styled.div`
+  margin: 50px 20px;
+  input {
+    border-radius: 4px;
+    cursor: pointer;
+    border: 1px solid grey;
+    height: 25px;
+    width: 25vw;
+  }
+  button {
+    width: 120px;
+    height: 35px;
+    border-radius: 4px;
+    border: 1px solid grey;
+    cursor: pointer;
+    margin: 10px 0px 20px 0px;
+    font-size: 18px;
+  }
+  button:hover {
+    color: grey;
+    background-color: black;
+  }
+`;
 
 
 export default function Login() {
@@ -16,25 +41,27 @@ export default function Login() {
     console.log(handleLoginAuth)
     return !isAuth ? (
       <div>
-        <label htmlFor="emailid">EMAIL ID</label>
-        <input
-          type="text"
-          value={emailid}
-          onChange={(e) => setEmailid(e.target.value)}
-        />
-        <br />
-        <br />
+        <FORMDATA>
+          <p>EMAIL ID</p><br />
+          <input
+            type="text"
+            value={emailid}
+            onChange={(e) => setEmailid(e.target.value)}
+          />
+          <br />
+          <br />
 
-        <label htmlFor="password">PASSWORD</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <p>PASSWORD</p><br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <br />
-        <br />
-        <button onClick={handleLogin}>{isAuth ? "LOGOUT" : "LOGIN"}</button>
+          <br />
+          <br />
+          <button onClick={handleLogin}>{isAuth ? "LOGOUT" : "LOGIN"}</button>
+        </FORMDATA>
       </div>
     ) : (
       <Redirect to="/" />
--- src/Components/Survey/Survey.jsx
+++ src/Components/Survey/Survey.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import styled from "styled-components"
+
+  const FORMDATA = styled.div`
+    margin: 50px 20px;
+    input {
+      border-radius: 4px;
+      cursor: pointer;
+      border: 1px solid grey;
+      height: 25px;
+      width: 25vw;
+    }
+    button {
+      width: 120px;
+      height: 35px;
+      border-radius: 4px;
+      border: 1px solid grey;
+      cursor: pointer;
+      margin: 10px 0px 20px 0px;
+      font-size: 18px;
+    }
+    button:hover {
+      color: grey;
+      background-color: black;
+    }
+  `;
 
 export default function Survey() {
     return (
-        <div>
-            SURVEY FORM
-        </div>
-    )
+      <div>
+        <FORMDATA>
+          <form action="">
+            <p htmlFor="firstname">FIRST NAME</p>
+            <input type="text" />
+            <br />
+            <p htmlFor="lastname">LAST NAME</p>
+            <input type="text" />
+            <br />
+            <p htmlFor="mobile">MOBILE NO</p>
+            <input type="text" />
+            <br />
+            <p htmlFor="emailid">EMAIL ID</p>
+            <input type="text" />
+            <br />
+            <p htmlFor="portfolio">PROTFOLIO</p>
+            <input type="text" />
+            <br />
+            <p htmlFor="occupation">OCCUPATION</p>
+            <input type="text" />
+            <br />
+            <button>ADD</button>
+          </form>
+        </FORMDATA>
+      </div>
+    );
 }
